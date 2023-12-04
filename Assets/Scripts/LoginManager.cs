d80788812e385bcca6e30d01224310b1595e0cf4
sangbeen0912 <sangbeen0912@gmail.com>
2023-12-04 15:24:43 +0900

    Feat: 중간저장

--- Assets/Scripts/LoginManager.cs
+++ Assets/Scripts/LoginManager.cs
@@ -7,10 +7,15 @@ public class LoginManager : MonoBehaviour
     public GameObject LoginCanvas;
     public GameObject KakaoCanvas;
     public GameObject UnagreePanel;
+    public GameObject AccountCanvas;
+    public GameObject ServicePanel;
+    public GameObject ServicePanel1;
     // Start is called before the first frame update
     void Start()
     {
-        KakaoCanvas.SetActive(false);       
+        KakaoCanvas.SetActive(false);
+        AccountCanvas.SetActive(false);
+        ServicePanel1.SetActive(false);
     }
 
     public void OnClickKakaobutton()
@@ -24,6 +29,18 @@ public class LoginManager : MonoBehaviour
     {
         LoginCanvas.SetActive(true);
         KakaoCanvas.SetActive(false);  
+    }
+
+    public void OnClickAccountbutton()
+    {
+        LoginCanvas.SetActive(false);
+        AccountCanvas.SetActive(true);
+    }
+    
+    public void OnClickCheckbutoon()
+    {
+        ServicePanel.SetActive(false);
+        ServicePanel1.SetActive(true);
     }
 
 }
